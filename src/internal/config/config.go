ebf9b187ad7041547472c6452aedebb71918523a
Maksim Dachevsky <maxdachevsky4@gmail.com>
2026-07-14 16:44:15 +0300

    stage 1

---
+++ src/internal/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Env string `yaml:"env"`
+}
+
+func Load(path string) (*Config, error) {
+	config := Config{}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("Error in reading file: %w", err)
+	}
+	err = yaml.Unmarshal(data, config)
+	if err != nil {
+		return nil, fmt.Errorf("Error in unmarshalling configuration: %w", err)
+	}
+	return &config, nil
+}
